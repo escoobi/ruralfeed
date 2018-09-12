8dc3842255f4fcb561a2c7f7c5e0ffbfe88ee4a5
Gustavo O. Cardozo <gustavo_o_@hotmail.com>
2018-09-12 18:15:50 -0400

    sadas

--- app/src/main/java/br/com/ruralfeed/ruralfeed/MainActivity.java
+++ app/src/main/java/br/com/ruralfeed/ruralfeed/MainActivity.java
@@ -1,5 +1,9 @@
 package br.com.ruralfeed.ruralfeed;
 
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,11 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tora();
+
+
     }
 
-    public void tora (){
-        String pneu = "oo";
-        pneu = pneu + " rr";
+
+    public class ProdutosAssincronos extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // Método que busca produtos assíncronamente.
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            // Método que envia os produtos encontrados para a main thread.
+
+        }
     }
+//https://medium.com/android-dev-br/trabalhando-com-tarefas-em-background-3d4da889ddfa
+    public class TarefaAssincronaIntentService extends IntentService {
+
+        public TarefaAssincronaIntentService() {
+            super("name");
+        }
+
+        @Override
+        protected void onHandleIntent(Intent intent) {
+            // Dentro desse método será implementada a execução da tárefa.
+        }
+    }
+
 }
