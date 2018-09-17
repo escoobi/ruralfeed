d688ed9f16748078efaf70dcc77b05a85c34e6d1
Gustavo O. Cardozo <gustavo_o_@hotmail.com>
2018-09-17 16:50:15 -0400

    sadas

--- app/src/main/java/br/com/ruralfeed/ruralfeed/MainActivity.java
+++ app/src/main/java/br/com/ruralfeed/ruralfeed/MainActivity.java
@@ -1,8 +1,12 @@
 package br.com.ruralfeed.ruralfeed;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,10 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this, exemploIntentService.class);
         startService(intent);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); //bloqueia orientação de tela.
+
         //**************************
 
         Intent intentTipoPessoa = new Intent(this, TipoPessoa.class);
         startService(intentTipoPessoa);
 
+        Button btnIncluir = (Button) findViewById(R.id.buttonIncluirRuralFeed);
+        btnIncluir.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View view) {
+                                              Intent intentBotao = new Intent(MainActivity.this, CadPessoaActivity.class);
+                                              startActivity(intentBotao);
+                                          }
+                                      }
+
+        );
     }
 }
